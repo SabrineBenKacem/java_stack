6cc0f3e0d447cbb2f0b0ff60af74f26af79fd89e
Sabrine Ben Kacem <sabrinebenkacem@gmail.com>
2023-10-09 21:53:05 +0100

    DaikichiPathVariables1

---
+++ Week1/Day 5/Core/DaikichiPathVariables1/src/main/java/com/sabrine/daikichipathvariables/DaikichiPathVariables1Application.java
@@ -0,0 +1,13 @@
+package com.sabrine.daikichipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiPathVariables1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariables1Application.class, args);
+	}
+
+}
